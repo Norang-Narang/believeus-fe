2d38eb995a10162612a6b7bff7e10f63d6c979ac
Monixc <chris13404@gmail.com>
2025-02-16 15:08:36 +0900

    feat: Layout 컴포넌트 적용
    - App 컴포넌트에 Layout 래퍼 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
+    <Layout>
       <h1>Believeus</h1>
-    </div>
+    </Layout>
   );
 }
 
