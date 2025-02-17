c5150aac12ea953415034ea4148d9b609aaafe80
Monixc <chris13404@gmail.com>
2025-02-18 03:23:20 +0900

    feat: Navbar 적용 및 라우팅 임의 세팅

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import Checkbox from "./components/common/Checkbox";
-import Dropdown from "./components/common/Dropdown";
-import Input from "./components/common/Input";
-import Button from "./components/common/Button";
-import { IoAdd } from "react-icons/io5";
+import Home from "./pages/Home";
+import { PATH } from "./constants/path";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: PATH.HOME,
+        element: <Home />,
+      },
+      {
+        path: PATH.MATCHING,
+        element: <div>매칭</div>,
+      },
+      {
+        path: PATH.WORK_CONDITION,
+        element: <div>근무조건</div>,
+      },
+      {
+        path: PATH.MY_PAGE,
+        element: <div>마이페이지</div>,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <Layout>
-      <h1>Believeus</h1>
-      <Checkbox label="체크박스" onChange={() => {}} />
-      <Dropdown
-        label="드롭다운"
-        options={[
-          {
-            label: "옵션 1",
-            value: "option1",
-          },
-          {
-            label: "옵션 2",
-            value: "option2",
-          },
-          {
-            label: "옵션 3",
-            value: "option3",
-          },
-        ]}
-        onChange={() => {}}
-      />
-      <Input label="인풋" onChange={() => {}} placeholder="플레이스홀더" />
-      <Button color="primary" size="large" onClick={() => {}}>
-        버튼1
-      </Button>
-      <Button
-        color="secondary"
-        style="outlined"
-        size="large"
-        onClick={() => {}}>
-        버튼2
-      </Button>
-      <Button
-        color="secondary"
-        icon={<IoAdd size={24} />}
-        size="large"
-        style="contained"
-        type="icon-only"
-      />
-    </Layout>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import styles from "./Layout.module.css";
+import Navbar from "../common/Navbar";
+import { NAV_ITEMS } from "../../constants/menu";
 
-const Layout = ({ children }) => {
+const Layout = () => {
+  const location = useLocation();
+  const [activeItem, setActiveItem] = useState(() => {
+    const currentPath = location.pathname;
+    const currentItem = NAV_ITEMS.find((item) => item.path === currentPath);
+    return currentItem?.label || "홈";
+  });
+
+  useEffect(() => {
+    const currentItem = NAV_ITEMS.find(
+      (item) => item.path === location.pathname
+    );
+    if (currentItem) {
+      setActiveItem(currentItem.path);
+    }
+  }, [location.pathname]);
+
+  const handleNavItemClick = (item) => {
+    setActiveItem(item.path);
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.statusBar}>topNav</div>
       </header>
-      <main className={styles.main}>{children}</main>
-      <footer className={styles.footer}>bottomNav</footer>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <footer className={styles.footer}>
+        <Navbar
+          items={NAV_ITEMS}
+          activeItem={activeItem}
+          onItemClick={handleNavItemClick}
+        />
+      </footer>
     </div>
   );
 };
